68a796ba262164d67b6b238a0da55a5a89cfa7f5
Ashutosh Ulape <ashuulape@gmail.com>
2025-11-05 21:58:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+
+
+ 
+ async src="https://www.googletagmanager.com/gtag/js?id=G-S867BNQ5F6"
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-S867BNQ5F6');
+
+
 const apiKey = '45dbb14c7bb84b8bb943f1b1bbd3a093';
 const baseUrl = 'https://api.rawg.io/api/games';
 const categories = [
