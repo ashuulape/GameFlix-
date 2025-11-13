767dc9b85aabe6793c9cd6b84503956212653500
Ashutosh Ulape <ashuulape@gmail.com>
2025-11-13 16:03:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
   gtag('config', 'G-S867BNQ5F6');
 
 
-const apiKey = '1c28457b558b4da19bc43d45c7764b3b';
+const apiKey = '21dbc3bad31144d6a46501c6b40ffbc9';
 const baseUrl = 'https://api.rawg.io/api/games';
 const categories = [
     { id: 4, name: 'Action' },
